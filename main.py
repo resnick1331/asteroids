bbf86d1bc0cb7c167b4e2f6df40dd40257500257
resnick1331 <andresleal7@gmail.com>
2024-12-12 04:04:23 +0000

    initial files for asteroids game

---
+++ main.py
@@ -0,0 +1,22 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+	pygame.init
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+			pygame.Surface.fill(screen, (1,1,1))
+
+
+if __name__ == "__main__":
+    main()
