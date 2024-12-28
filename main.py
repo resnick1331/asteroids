1178f145ae36c8967c13d1936fc1424fc380efad
resnick1331 <andresleal7@gmail.com>
2024-12-28 21:44:34 +0000

    review outputs

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ def main():
 	Shot.containers = (updatable, drawable, bullets)
 	
 	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
